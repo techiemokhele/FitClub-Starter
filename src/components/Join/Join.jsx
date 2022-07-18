b443aba9fdf6c623564bb762e4b391310d2ad12c
techiemokhele <neomokhele23@gmail.com>
2022-07-18 05:43:29 +0200

    Join us section complete with email config

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 //import customs
 import "./Join.css";
 
 function Join() {
-  return <div>Join</div>;
+  const form = useRef();
+
+  //email send code
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">ready to</span>
+          <span>level up</span>
+        </div>
+        <div>
+          <span>your body</span>
+          <span className="stroke-text">with us?</span>
+        </div>
+      </div>
+
+      <div className="right-j">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
+          <input
+            name="user_email"
+            type="email"
+            placeholder="example@gmail.com"
+          />
+          <button className="btn btn-j" type="submit">
+            Join Now
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Join;
